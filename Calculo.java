9740c7e30dd3d639ce250d493bd1558b1c995073
unknown <dieste1254@gmail.com>
2023-05-27 12:59:10 -0300

    Deixando a pessoa decidir a nota

--- Calculo.java
+++ Calculo.java
@@ -1,21 +1,29 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Calculo {
 
 
 
     public static void main(String args[]){
-    media();
+        System.out.println("Digite a primeira nota: ");
+        Scanner num1 = new Scanner(System.in);
+        int nota1 = num1.nextInt();
+        System.out.println("Digite a segunda nota: ");
+        Scanner num2 = new Scanner(System.in);
+        int nota2 = num2.nextInt();
+        System.out.println("Digite a terceira nota: ");
+        Scanner num3 = new Scanner(System.in);
+        int nota3 = num3.nextInt();
+        System.out.println("Digite a quarta nota: ");
+        Scanner num4 = new Scanner(System.in);
+        int nota4 = num4.nextInt();
 
-
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A média é:");
+        System.out.println(media);
     }
 
-    private static void media(){
-        int nota1 = 8;
-        int nota2 = 6;
-        int nota3 = 4;
-        int nota4 = 6;
 
-        int soma = nota1 + nota2 + nota3 + nota4;
-        int fim = soma / 4;
-        System.out.println("A média final é:" + fim);
-    }
+
 }
